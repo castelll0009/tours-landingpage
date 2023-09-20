18ba044ae9d87606e196552893878e95e4a5d053
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-09-20 15:30:39 -0500

    i added one msg emergent
    se agrego una carpertas css>Section1>msg-emegent.css
    js>Section1>msg-emegent.js

---
+++ js/Section1/msg-emegent.js
@@ -0,0 +1,44 @@
+// This isn't necessary but more for demonstration purposes
+
+const typSpd = 70; 
+const waitTime = 500;
+
+const text = [
+  "I would like to visit the Amazon",
+  "Oh i like to priorize the birding",
+  "En donde puedo perzonalizar mi tour?",
+  "i would like to talk whit one asesor",
+  "Necesito mas informacion please!"
+]
+
+var mi = 0;
+
+function writeString(e, str, i) {
+  e.innerHTML = e.innerHTML + str[i];
+  
+  if (e.innerHTML.length == str.length && mi != text.length)
+    setTimeout(slowlyDelete, waitTime, e);
+}
+
+function deleteString(e) {
+  e.innerHTML = e.innerHTML.substring(0, e.innerHTML.length - 1);
+  
+  if (e.innerHTML.length == 0)
+    slowlyWrite(e, text[mi++]);
+}
+
+function slowlyDelete(e) {
+  for (var i = 0; i < e.innerHTML.length; i++) {
+    setTimeout(deleteString, typSpd / 2 * i, e);
+  }
+}
+
+function slowlyWrite(e, str) {
+  for (var i = 0; i < str.length; i++) {
+    setTimeout(writeString, typSpd * i, e, str, i);
+  }
+}
+
+const msg = document.querySelector(".msg-icn");
+
+slowlyDelete(msg);
