87a34eb146f8f8bceb743798664aac4f6799daa0
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-09-28 13:35:59 -0500

    msg emergent

--- js/Section1/msg-emegent.js
+++ js/Section1/msg-emegent.js
@@ -1,35 +1,32 @@
-// This isn't necessary but more for demonstration purposes
-
-const typSpd = 30; 
+const typSpd = 30;
 const waitTime = 2000;
 
 const text = [
+  "Where can I customize my tour?",
   "I would like to visit the Amazon",
-  "Oh i like to priorize the birding",
-  "En donde puedo perzonalizar mi tour?",
-  "i would like to talk whit one asesor",
-  "Necesito mas informacion please!"
-]
+  "Oh, I like to prioritize birding",
+  "¿En dónde puedo personalizar mi tour?",
+  "I would like to talk with one advisor",
+  "I need more info please" // Actualizado
+];
 
 var mi = 0;
 
 function writeString(e, str, i) {
   e.innerHTML = e.innerHTML + str[i];
-  
-  if (e.innerHTML.length == str.length && mi != text.length){
-    setTimeout(slowlyDelete, waitTime, e);
-  }else{
-    
-  }
-   
 
+  if (e.innerHTML.length == str.length && mi != text.length) {
+    setTimeout(slowlyDelete, waitTime, e);
+  }
 }
 
 function deleteString(e) {
   e.innerHTML = e.innerHTML.substring(0, e.innerHTML.length - 1);
-  
-  if (e.innerHTML.length == 0)
+
+  if (e.innerHTML.length == 0) {
     slowlyWrite(e, text[mi++]);
+    if (mi === text.length) mi = 0; // Reiniciar el índice al final del arreglo
+  }
 }
 
 function slowlyDelete(e) {
@@ -46,4 +43,13 @@ function slowlyWrite(e, str) {
 
 const msg = document.querySelector(".msg-icn");
 
-slowlyDelete(msg);
+function startTyping() {
+  mi = 0; // Reiniciar el índice
+  slowlyDelete(msg);
+}
+
+// Llamar a la función inicialmente
+startTyping();
+
+// Repetir el proceso cada vez que se complete
+setInterval(startTyping, (text.length * waitTime + text.length * typSpd * text[0].length) * text.length);
