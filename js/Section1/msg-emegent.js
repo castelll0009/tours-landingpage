d70cf489c5a0481b530f53948b7d6cea5af0ae4a
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-09-28 16:26:24 -0500

    Update msg-emegent.js
    the msg-emergent was fixed

--- js/Section1/msg-emegent.js
+++ js/Section1/msg-emegent.js
@@ -2,12 +2,11 @@ const typSpd = 30;
 const waitTime = 2000;
 
 const text = [
-  "Where can I customize my tour?",
   "I would like to visit the Amazon",
   "Oh, I like to prioritize birding",
-  "¿En dónde puedo personalizar mi tour?",
+  "En donde puedo personalizar mi tour?",
   "I would like to talk with one advisor",
-  "I need more info please" // Actualizado
+  "Necesito más información, por favor!"
 ];
 
 var mi = 0;
@@ -16,7 +15,12 @@ function writeString(e, str, i) {
   e.innerHTML = e.innerHTML + str[i];
 
   if (e.innerHTML.length == str.length && mi != text.length) {
-    setTimeout(slowlyDelete, waitTime, e);
+    setTimeout(() => slowlyDelete(e), waitTime);
+  } else if (e.innerHTML.length == str.length && mi == text.length) {
+    mi = 0;
+    setTimeout(() => slowlyDelete(e), waitTime);
+  } else {
+    setTimeout(() => writeString(e, str, i + 1), typSpd);
   }
 }
 
@@ -25,31 +29,25 @@ function deleteString(e) {
 
   if (e.innerHTML.length == 0) {
     slowlyWrite(e, text[mi++]);
-    if (mi === text.length) mi = 0; // Reiniciar el índice al final del arreglo
+  } else {
+    setTimeout(() => deleteString(e), typSpd / 2);
   }
 }
 
 function slowlyDelete(e) {
-  for (var i = 0; i < e.innerHTML.length; i++) {
-    setTimeout(deleteString, typSpd / 2 * i, e);
+  if (e.innerHTML.length > 0) {
+    deleteString(e);
+  } else {
+    setTimeout(() => slowlyWrite(e, text[mi++]), waitTime);
   }
 }
 
 function slowlyWrite(e, str) {
-  for (var i = 0; i < str.length; i++) {
-    setTimeout(writeString, typSpd * i, e, str, i);
+  if (str) {
+    writeString(e, str, 0);
   }
 }
 
 const msg = document.querySelector(".msg-icn");
 
-function startTyping() {
-  mi = 0; // Reiniciar el índice
-  slowlyDelete(msg);
-}
-
-// Llamar a la función inicialmente
-startTyping();
-
-// Repetir el proceso cada vez que se complete
-setInterval(startTyping, (text.length * waitTime + text.length * typSpd * text[0].length) * text.length);
+slowlyDelete(msg);
