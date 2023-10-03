b76c45103b06e56e1d7d3922963bfbb27f686aed
sismales <sie7effxxkk@outlook.es>
2023-10-02 22:03:51 -0500

    card
    add short text , buttun and media resolution

--- Section2/article.js
+++ Section2/article.js
@@ -1,10 +1,18 @@
 function loadDoc() {
    const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-      document.getElementById("art-text").innerHTML =
+      document.getElementById("#art-text").innerHTML =
       this.responseText;
     }
     xhttp.open("GET","amazon.txt");
     xhttp.send();
    
   }
+
+  $(document).jquery(function(){
+    $("btn-pe").on("click",function(){
+      $("#art.text").load("amazon.txt");
+    });
+  });
+
+  
