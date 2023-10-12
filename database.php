3afd42a5d07a20670987445b633fd020192a87a7
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-10-12 12:30:32 -0400

    a

--- database.php
+++ database.php
@@ -1,16 +1,21 @@
 <?php
 
-$connection = mysqli_connect(
-  'localhost', 'root', 'password', 'caracara_db'
-);
+// $connection = mysqli_connect(
+//   'localhost', 'root', 'password', 'caracara_db'
+// );
+
+
+$host = '162.241.61.135'; // Puede ser una dirección IP o un nombre de dominio
+$database = 'caracar1_caracara_db';
+$username = 'caracar1_castell';
+$password = 'castell1997';
+
+$connection = mysqli_connect($host, $username, $password, $database);
+
 
 //for testing connection
 if($connection) {
- echo 'database is connected';
-}
-
-// $host = '162.241.61.135'; // Puede ser una dirección IP o un nombre de dominio
-// $database = 'caracar1_caracara_db';
-// $username = 'caracar1_castell';
-// $password = 'castell1997';
+  echo 'database is connected';
+ }
+ 
 ?>
