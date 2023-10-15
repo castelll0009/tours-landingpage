fc8989de7f7ce9b5ed884e4b60413ba89102ff40
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-10-14 22:13:14 -0400

    nice

--- js/section-2-swiper-slide.js
+++ js/section-2-swiper-slide.js
@@ -16,16 +16,4 @@ var swiper = new Swiper(".swiper", {
       el: ".swiper-pagination"
     }
   });
-    // Función para generar un color aleatorio
-    function getRandomColor() {
-      var colors = ["#087ac4", "#b45205", "#86c706"];
-      var randomIndex = Math.floor(Math.random() * colors.length);
-      return colors[randomIndex];
-  }
-
-  // Aplicar un color de fondo aleatorio a todos los elementos con la clase "custom-button"
-  var buttons = document.querySelectorAll(".custom-button");
-  buttons.forEach(function(button) {
-      var randomColor = getRandomColor();
-      button.style.background = randomColor;
-  });
+  
