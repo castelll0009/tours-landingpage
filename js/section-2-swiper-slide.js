745cb0a438d4f3f02a2ea2e30fb4b6a4c149bb23
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-10-27 23:26:44 -0400

    Swiper slider count

--- js/section-2-swiper-slide.js
+++ js/section-2-swiper-slide.js
@@ -1,4 +1,6 @@
-var swiper = new Swiper(".swiper", {
+// Espera a que el documento esté listo
+$(document).ready(function() {
+  var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
@@ -16,4 +18,32 @@ var swiper = new Swiper(".swiper", {
       el: ".swiper-pagination"
     }
   });
-  
+
+  // Mantén el número de slide actual en un rango entre 0 y totalSlides - 1
+  var currentSlide = 0;
+
+  // Función para actualizar el número de slide
+  function updateSlideNumber() {
+    var totalSlides = swiper.slides.length; // Obtiene la cantidad total de slides
+    $("#slide-number").html((currentSlide + 1).toString().padStart(2, "0"));
+  }
+
+  // Agrega un event listener para el botón "Anterior"
+  $(".prev-slide").click(function() {
+    var totalSlides = swiper.slides.length; // Obtiene la cantidad total de slides
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides; // Resta 1 y ajusta al rango
+    swiper.slideTo(currentSlide);
+    updateSlideNumber();
+  });
+
+  // Agrega un event listener para el botón "Siguiente"
+  $(".next-slide").click(function() {
+    var totalSlides = swiper.slides.length; // Obtiene la cantidad total de slides
+    currentSlide = (currentSlide + 1) % totalSlides; // Suma 1 y ajusta al rango
+    swiper.slideTo(currentSlide);
+    updateSlideNumber();
+  });
+
+  // Actualiza el número de slide al cargar la página
+  updateSlideNumber();
+});
